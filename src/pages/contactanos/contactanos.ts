8b18dd009f515f4c899565c77e64d83026b66bcf
user143988 <user143988@74-80-245-194.ic2net.net>
2018-08-08 15:15:11 -0700

    contactanos

--- src/pages/contactanos/contactanos.ts
+++ src/pages/contactanos/contactanos.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+declare var google: any;
 
 /**
  * Generated class for the ContactanosPage page.
@@ -14,12 +16,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'contactanos.html',
 })
 export class ContactanosPage {
-
+  @ViewChild('map') mapRef: ElementRef;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactanosPage');
+  ionViewDidLoad(){
+    this.showMap();
   }
+  
+  showMap(){
+    const location= new google.maps.LatLng(-2.1824565,-79.8930946);
+  
+    const options= {
+        center: location,
+        zoom: 14
+    };
+    
+    const map= new google.maps.Map(this.mapRef.nativeElement, options);
+    
+    this.addMarker(location, map);
+  }
+
+    addMarker(position, map){
+        return new google.maps.Marker({
+            position,
+            map
+        });
+    }
 
 }
